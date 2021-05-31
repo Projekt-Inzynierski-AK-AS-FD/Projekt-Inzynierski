4c16b61eb1f7ac170142787b12bd7cee317316d6
haribo841 <adamwaldemarkubis@gmail.com>
2021-05-31 10:04:41 +0200

    Podpięcie kalkulatora

--- Abituria szkielet/Abituria szkielet/Calculator.xaml.cs
+++ Abituria szkielet/Abituria szkielet/Calculator.xaml.cs
@@ -22,6 +22,112 @@ namespace Abituria_szkielet
         public Calculator()
         {
             InitializeComponent();
+            ResultText.Text = string.Empty;
+            CurrentOperationText.Text = string.Empty;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultText.Text = string.Empty;
+
+            var button = sender as Button;
+
+            var currentNumber = button.Name[button.Name.Length - 1];
+
+            CurrentOperationText.Text += currentNumber;
+        }
+
+        private void Button_ClickDodawanie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperationText.Text += "+";
+        }
+
+        private void Button_ClickOdejmowanie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperationText.Text += "-";
+        }
+
+        private void Button_ClickMnozenie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperationText.Text += "*";
+        }
+
+        private void Button_ClickDzielenie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperationText.Text += ":";
+        }
+
+        private void Button_ClickWynik(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            ResultText.Text = CalculateResult(operation).ToString();
+
+            CurrentOperationText.Text = string.Empty;
+        }
+
+        private bool ContainsOperation(string operation)
+            => operation.Contains('+') || operation.Contains('-') || operation.Contains('*') || operation.Contains(':');
+
+        private long CalculateResult(string operation)
+        {
+            if (operation.Contains('+'))
+            {
+                var elements = operation.Split('+');
+
+                return long.Parse(elements[0]) + long.Parse(elements[1]);
+            }
+
+            if (operation.Contains('-'))
+            {
+                var elements = operation.Split('-');
+
+                return long.Parse(elements[0]) - long.Parse(elements[1]);
+            }
+
+            if (operation.Contains('*'))
+            {
+                var elements = operation.Split('*');
+
+                return long.Parse(elements[0]) * long.Parse(elements[1]);
+            }
+
+            if (operation.Contains(':'))
+            {
+                var elements = operation.Split(':');
+
+                return long.Parse(elements[0]) / long.Parse(elements[1]);
+            }
+
+            return default;
         }
     }
 }
