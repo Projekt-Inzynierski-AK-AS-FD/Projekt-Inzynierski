e04c7fabc91fb9604fe514850c95beaef299578a
Aleksandra <48011791+astolowska@users.noreply.github.com>
2021-09-07 17:33:24 +0200

    Add files via upload

---
+++ Abituria szkielet/Abituria szkielet/Abituria szkielet/Calculator.xaml.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Abituria
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+
+    public partial class Calculator : Window
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+            ResultText.Text = "0";
+            CurrentOperationText.Text = string.Empty;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultText.Text = string.Empty;
+
+            var button = sender as Button;
+
+            var currentNumber = button.Name[button.Name.Length - 1];
+
+            CurrentOperationText.Text += currentNumber;
+        }
+        private void Button_ClickDodawanie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            if(string.IsNullOrEmpty(CurrentOperationText.Text))
+            {
+                CurrentOperationText.Text = ResultText.Text + "+";
+            }
+            else
+            {
+                CurrentOperationText.Text += "+";
+            }
+            
+        }
+        private void Button_ClickOdejmowanie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            if (string.IsNullOrEmpty(CurrentOperationText.Text))
+            {
+                CurrentOperationText.Text = ResultText.Text + "-";
+            }
+            else
+            {
+                CurrentOperationText.Text += "-";
+            }
+        }
+        private void Button_ClickMnozenie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            if (string.IsNullOrEmpty(CurrentOperationText.Text))
+            {
+                CurrentOperationText.Text = ResultText.Text + "*";
+            }
+            else
+            {
+                CurrentOperationText.Text += "*";
+            }
+        }
+        private void Button_ClickDzielenie(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            if (string.IsNullOrEmpty(CurrentOperationText.Text))
+            {
+                CurrentOperationText.Text = ResultText.Text + ":";
+            }
+            else
+            {
+                CurrentOperationText.Text += ":";
+            }
+        }
+        private void Button_ClickPierwiastek(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperationText.Text += "√";
+        }
+        private void Button_ClickPotega(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperationText.Text += "²";
+        }
+        private void Button_ClickUlamek(object sender, RoutedEventArgs e)
+        {
+            CurrentOperationText.Text = "1/" + CurrentOperationText.Text;
+            var operation = CurrentOperationText.Text;
+
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+        }
+        private void Button_ClickWynik(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+            ResultText.Text = CalculateResult(operation).ToString();
+
+            CurrentOperationText.Text = string.Empty;
+        }
+
+        private bool ContainsOperation(string operation)
+            => operation.Contains('+') || operation.Contains('-') || operation.Contains('*') || operation.Contains(':');
+
+        private double OperationAfterOperation(string operation)
+        {
+            if (operation.Contains('+'))
+            {
+                var elements = operation.Split('+');
+
+                return double.Parse(elements[0]) + double.Parse(elements[1]);
+            }
+
+            return default;
+        }
+
+        private double CalculateResult(string operation)
+        {
+            if (operation.Contains('+'))
+            {
+                var elements = operation.Split('+');
+
+                if (String.IsNullOrEmpty(elements[1]))
+                {
+                    elements[1] = elements[0];
+                }
+
+                return double.Parse(elements[0]) + double.Parse(elements[1]);
+            }
+
+            if (operation.Contains('-'))
+            {
+                var elements = operation.Split('-');
+
+                if (String.IsNullOrEmpty(elements[1]))
+                {
+                    elements[1] = elements[0];
+                }
+
+                return double.Parse(elements[0]) - double.Parse(elements[1]);
+            }
+
+            if (operation.Contains('*'))
+            {
+                var elements = operation.Split('*');
+
+                if (String.IsNullOrEmpty(elements[1]))
+                {
+                    elements[1] = elements[0];
+                }
+
+                return double.Parse(elements[0]) * double.Parse(elements[1]);
+            }
+
+            if (operation.Contains(':'))
+            {
+                var elements = operation.Split(':');
+
+                if (String.IsNullOrEmpty(elements[1]))
+                {
+                    elements[1] = elements[0];
+                }
+
+                if (elements[1] == "0")
+                {
+                    return 0;
+                }
+                else
+                {
+                    return double.Parse(elements[0]) / double.Parse(elements[1]);
+                }
+            }
+
+            if (operation.Contains('/'))
+            {
+                var elements = operation.Split('/');
+
+                if (String.IsNullOrEmpty(elements[1]))
+                {
+                    elements[1] = elements[0];
+                }
+
+                return double.Parse(elements[0]) / double.Parse(elements[1]);
+            }
+
+            if (operation.Contains('√'))
+            {
+                var elements = operation.Split('√');
+
+
+                if (String.IsNullOrEmpty(elements[0]))                          //Jeśli nic nie ma przed znakiem działania (.Split(...))
+                {
+                    return Math.Sqrt(double.Parse(elements[1]));
+                }
+                else
+                {
+                    return double.Parse(elements[0]) * Math.Sqrt(double.Parse(elements[1]));
+                }
+            }
+
+            if (operation.Contains('²'))
+            {
+                var elements = operation.Split('²');
+
+                return Math.Pow(double.Parse(elements[0]),2);
+            }
+
+            if (operation.Contains('²'))
+            {
+                var elements = operation.Split('²');
+
+                return Math.Pow(double.Parse(elements[0]), 2);
+            }
+
+            return default;
+        }
+
+    }
+}
---
+++ Abituria szkielet/Abituria szkielet/Abituria szkielet/PageMatura2021.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Abituria
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PageMatura.xaml
+    /// </summary>
+    public partial class PageMatura2021 : Page
+    {
+        public PageMatura2021()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAbituria(object sender, RoutedEventArgs e)
+        {
+            var mainWin = new MainWindow();
+            mainWin.Show();
+
+        }
+
+        private void ButtonKalkulator(object sender, RoutedEventArgs e)
+        {
+            var calculator = new Calculator();
+            calculator.Show();
+        }
+
+        private void ButtonMatura(object sender, RoutedEventArgs e)
+        {
+
+            //MaturaFrame.NavigationService.Navigate(new Uri("PageMatura.xaml", UriKind.Relative));
+            //MaturaFrame.NavigationService.Navigate(new PageMatura());
+            //MaturaFrame.Content = new PageMatura();
+
+            PageMaturaLata pageMaturaLata = new PageMaturaLata();
+            NavigationService.Navigate(pageMaturaLata);
+        }
+
+        private void ButtonDzialy(object sender, RoutedEventArgs e)
+        {
+            //przełączenie z jednej strony (matury) na inną
+            PageDzialy pageDzialy = new PageDzialy();
+            NavigationService.Navigate(pageDzialy);
+
+
+        }
+
+        private void ButtonZadania(object sender, RoutedEventArgs e)
+        {
+            PageZadania pageZadania = new PageZadania();
+            NavigationService.Navigate(pageZadania);
+        }
+
+        private void ButtonWideo(object sender, RoutedEventArgs e)
+        {
+            PageWideo pageWideo = new PageWideo();
+            NavigationService.Navigate(pageWideo);
+        }
+        private void Button2021(object sender, RoutedEventArgs e)
+        {
+            PageMatura2021 pageMatura2021 = new PageMatura2021();
+            NavigationService.Navigate(pageMatura2021);
+        }
+        private void Button2020(object sender, RoutedEventArgs e)
+        {
+            PageMatura2020 pageMatura2020 = new PageMatura2020();
+            NavigationService.Navigate(pageMatura2020);
+        }
+        private void Button2019(object sender, RoutedEventArgs e)
+        {
+            PageMatura2019 pageMatura2019 = new PageMatura2019();
+            NavigationService.Navigate(pageMatura2019);
+        }
+    }
+}
