d883f79f4fa752a7cdc76c593d09a3738da7062c
Aleksandra <aleksastolow@gmail.com>
2021-10-31 03:59:18 +0100

    wyświetlanie postaci ogólnej łańcuch z ifów

--- Abituria/Abituria szkielet/CalcQuadraticFunc.xaml.cs
+++ Abituria/Abituria szkielet/CalcQuadraticFunc.xaml.cs
@@ -32,13 +32,17 @@ namespace Abituria
             double a = double.Parse(fieldA.Text);
             double b = double.Parse(fieldB.Text);
             double c = double.Parse(fieldC.Text);
-            FunKwadratOgolna(a, b, c);
+
+            //weryfikacja
+            if(a == 0)
+            {
+                MessageBox.Show("Pamiętaj, że w każdej funkcji kwadratowej współczynnik a jest liczbą rzeczywistą różną od 0!");
+                return;
+            }
 
 
-            // do zmiany tylko muszę wymyślić godne rozwiązanie
-            this.pOgolna.Text = $"y = {a}x2 + {b}x + {c}";
+            FunQuad(a, b, c);
 
-            //  case tu nie zadziała, chyba że zakombinuję, bo if byłby chory, aaaaaaaaaaaaaaaaaaaaaaaaaaa
 
         }
 
@@ -69,14 +73,15 @@ namespace Abituria
         }
         */
 
-        private void FunKwadratOgolna(double a, double b, double c)
+        private void FunQuad(double a, double b, double c)
         {
+            // obliczenia
             double delta = Math.Pow(b, 2) - (4 * a * c);
             double x0 = (-b) / (2 * a);
             double x1 = Math.Round(((-b) - Math.Sqrt(delta)) / (2 * a), 2);
             double x2 = Math.Round(((-b) + Math.Sqrt(delta)) / (2 * a), 2);
 
-
+            // wyświetlanie wyniku
             if (delta < 0)
             {
                 string resultTxt = "Δ < 0, funkcja nie posiada miejsc zerowych";
@@ -93,11 +98,158 @@ namespace Abituria
                 result.Text = resultTxt;
             }
 
+
             this.result.Visibility = Visibility.Visible;
-            
+            PosOgolnaShow(a, b, c);
+
 
         }
-
+        private void PosOgolnaShow(double a, double b, double c)
+        {
+            // wyświetlanie postaci ogólnej f. kwadratowej
+            // do poprawy przy b = -1 !
+            if (b > 0)
+            {
+                if (c > 0)
+                {
+                    if (a == 1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = x2 + x + {c}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = x2 + {b}x + {c}";
+                        }
+                    }
+                    else if (b == 1)
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 + x + {c}";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 + {b}x + {c}";
+                    }
+                }
+                else if (c < 0)
+                {
+                    if (a == 1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = x2 + x - {c * (-1)}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = x2 + {b}x - {c * (-1)}";
+                        }
+                    }
+                    else if (b == 1)
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 + x - {c * (-1)}";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 + {b}x - {c * (-1)}";
+                    }      
+                }
+                else if (c == 0)
+                {
+                    if (a == 1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = x2 + x";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = x2 + {b}x";
+                        }
+                    }
+                    else if (b == 1)
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 + x";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 + {b}x";
+                    }     
+                }
+            }
+            else if (b < 0)
+            {
+                if (c > 0)
+                {
+                    if(a == 1)
+                    {
+                        this.pOgolna.Text = $"y = x2 - {b * (-1)}x + {c}";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 - {b * (-1)}x + {c}";
+                    } 
+                }
+                else if (c < 0)
+                {
+                    if (a == 1)
+                    {
+                        this.pOgolna.Text = $"y = x2 - {b * (-1)}x - {c * (-1)}";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 - {b * (-1)}x - {c * (-1)}";
+                    }
+                }
+                else if (c == 0)
+                {
+                    if (a == 1)
+                    {
+                        this.pOgolna.Text = $"y = x2 - {b * (-1)}x";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 - {b * (-1)}x";
+                    } 
+                }
+            }
+            else if (b == 0)
+            {
+                if (c > 0)
+                {
+                    if (a == 1)
+                    {
+                        this.pOgolna.Text = $"y = x2 + {c}";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 + {c}";
+                    }    
+                }
+                else if (c < 0)
+                {
+                    if (a == 1)
+                    {
+                        this.pOgolna.Text = $"y = x2 - {c * (-1)}";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 - {c * (-1)}";
+                    }  
+                }
+                else if (c == 0)
+                {
+                    if (a == 1)
+                    {
+                        this.pOgolna.Text = $"y = x2";
+                    }
+                    else
+                    {
+                        this.pOgolna.Text = $"y = {a}x2";
+                    } 
+                }
+            }
+        }
     }
 }
 
