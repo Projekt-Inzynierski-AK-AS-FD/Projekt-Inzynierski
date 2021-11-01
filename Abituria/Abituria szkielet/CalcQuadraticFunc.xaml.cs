0efeedda987c62b02c3a58868daeea46d42ec4d3
Aleksandra <aleksastolow@gmail.com>
2021-11-01 21:29:19 +0100

    zaduszniki

--- Abituria/Abituria szkielet/CalcQuadraticFunc.xaml.cs
+++ Abituria/Abituria szkielet/CalcQuadraticFunc.xaml.cs
@@ -26,27 +26,39 @@ namespace Abituria
 
         private void ShowResult(object sender, RoutedEventArgs e)
         {
-            //zastąpić to casem po postawieniu też iloczynej i kanocznicznej, mlem
-            //i oczywiście trzeba dodać weryfikację
-            
-            double a = double.Parse(fieldA.Text);
-            double b = double.Parse(fieldB.Text);
-            double c = double.Parse(fieldC.Text);
+            // POBIERANIE INPUTU
+            // bezpieczniejsze niż double.Parse, lepiej weryfikuje i nie wyrzuca wyjątku w przypadku nieprawidłowej wartości
+            // z double.Parse trzeba by napisać wyjątek i wychodzi podobnie, bo i tak potrzeba weryfikacji, ale TryParse działa pewniej i jest zalecany
+            string valA = fieldA.Text;
+            string valB = fieldB.Text;
+            string valC = fieldC.Text;
+            double a, b, c = 0;
+            double.TryParse(valA, out a);
+            double.TryParse(valB, out b);
+            double.TryParse(valC, out c);
 
-            //weryfikacja
-            if(a == 0)
+            //weryfikacja poprawności wprowadzonych danych
+            if (a == 0)
             {
-                MessageBox.Show("Pamiętaj, że w każdej funkcji kwadratowej współczynnik a jest liczbą rzeczywistą różną od 0!");
+                MessageBox.Show("Pamiętaj, że w każdej funkcji kwadratowej współczynnik a jest liczbą rzeczywistą różną od 0! Spróbuj jeszcze raz.", "Nieprawidłowa wartość:");
+                Reset();
+                return;
+            }
+            if (double.TryParse(valA, out a) != true || double.TryParse(valB, out b) != true || double.TryParse(valC, out c) != true)
+            {
+                MessageBox.Show("Ups, coś poszło nie tak. Sprawdź, czy wprowadzone dane są prawidłowe i spróbuj jeszcze raz.", "Nieprawidłowa wartość:");
+                Reset();
                 return;
             }
 
-
             FunQuad(a, b, c);
 
-
         }
-
-        private void Reset(object sender, RoutedEventArgs e)
+        private void ResetBtn(object sender, RoutedEventArgs e)
+        {
+            Reset();
+        }
+        private void Reset()
         {
             fieldA.Text = "";
             fieldB.Text = "";
@@ -107,7 +119,6 @@ namespace Abituria
         private void PosOgolnaShow(double a, double b, double c)
         {
             // wyświetlanie postaci ogólnej f. kwadratowej
-            // do poprawy przy b = -1 !
             if (b > 0)
             {
                 if (c > 0)
@@ -121,6 +132,17 @@ namespace Abituria
                         else
                         {
                             this.pOgolna.Text = $"y = x2 + {b}x + {c}";
+                        }
+                    }
+                    else if (a == -1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = -x2 + x + {c}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = -x2 + {b}x + {c}";
                         }
                     }
                     else if (b == 1)
@@ -145,6 +167,17 @@ namespace Abituria
                             this.pOgolna.Text = $"y = x2 + {b}x - {c * (-1)}";
                         }
                     }
+                    else if (a == -1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = -x2 + x - {c * (-1)}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = -x2 + {b}x - {c * (-1)}";
+                        }
+                    }
                     else if (b == 1)
                     {
                         this.pOgolna.Text = $"y = {a}x2 + x - {c * (-1)}";
@@ -152,7 +185,7 @@ namespace Abituria
                     else
                     {
                         this.pOgolna.Text = $"y = {a}x2 + {b}x - {c * (-1)}";
-                    }      
+                    }
                 }
                 else if (c == 0)
                 {
@@ -167,6 +200,17 @@ namespace Abituria
                             this.pOgolna.Text = $"y = x2 + {b}x";
                         }
                     }
+                    else if (a == -1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = -x2 + x";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = -x2 + {b}x";
+                        }
+                    }
                     else if (b == 1)
                     {
                         this.pOgolna.Text = $"y = {a}x2 + x";
@@ -174,27 +218,71 @@ namespace Abituria
                     else
                     {
                         this.pOgolna.Text = $"y = {a}x2 + {b}x";
-                    }     
+                    }
                 }
             }
             else if (b < 0)
             {
                 if (c > 0)
                 {
-                    if(a == 1)
+                    if (a == 1)
                     {
-                        this.pOgolna.Text = $"y = x2 - {b * (-1)}x + {c}";
+                        if (b == -1)
+                        {
+                            this.pOgolna.Text = $"y = x2 - x + {c}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = x2 - {b * (-1)}x + {c}";
+                        }
+                    }
+                    else if (a == -1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = -x2 - x + {c}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = -x2 - {b * (-1)}x + {c}";
+                        }
+                    }
+                    else if (b == -1)
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 - x + {c}";
                     }
                     else
                     {
                         this.pOgolna.Text = $"y = {a}x2 - {b * (-1)}x + {c}";
-                    } 
+                    }
                 }
                 else if (c < 0)
                 {
                     if (a == 1)
                     {
-                        this.pOgolna.Text = $"y = x2 - {b * (-1)}x - {c * (-1)}";
+                        if (b == -1)
+                        {
+                            this.pOgolna.Text = $"y = x2 - x - {c * (-1)}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = x2 - {b * (-1)}x - {c * (-1)}";
+                        }
+                    }
+                    else if (a == -1)
+                    {
+                        if (b == 1)
+                        {
+                            this.pOgolna.Text = $"y = -x2 - x - {c * (-1)}";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = -x2 - {b * (-1)}x - {c * (-1)}";
+                        }
+                    }
+                    else if (b == -1)
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 - x - {c * (-1)}";
                     }
                     else
                     {
@@ -205,12 +293,34 @@ namespace Abituria
                 {
                     if (a == 1)
                     {
-                        this.pOgolna.Text = $"y = x2 - {b * (-1)}x";
+                        if (b == -1)
+                        {
+                            this.pOgolna.Text = $"y = x2 - x";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = x2 - {b * (-1)}x";
+                        }
+                    }
+                    else if (a == -1)
+                    {
+                        if (b == -1)
+                        {
+                            this.pOgolna.Text = $"y = -x2 - x";
+                        }
+                        else
+                        {
+                            this.pOgolna.Text = $"y = -x2 - {b * (-1)}x";
+                        }
+                    }
+                    else if (b == -1)
+                    {
+                        this.pOgolna.Text = $"y = {a}x2 - x";
                     }
                     else
                     {
                         this.pOgolna.Text = $"y = {a}x2 - {b * (-1)}x";
-                    } 
+                    }
                 }
             }
             else if (b == 0)
@@ -221,10 +331,14 @@ namespace Abituria
                     {
                         this.pOgolna.Text = $"y = x2 + {c}";
                     }
+                    else if (a == -1)
+                    {
+                        this.pOgolna.Text = $"y = -x2 + {c}";
+                    }
                     else
                     {
                         this.pOgolna.Text = $"y = {a}x2 + {c}";
-                    }    
+                    }
                 }
                 else if (c < 0)
                 {
@@ -232,10 +346,14 @@ namespace Abituria
                     {
                         this.pOgolna.Text = $"y = x2 - {c * (-1)}";
                     }
+                    else if (a == -1)
+                    {
+                        this.pOgolna.Text = $"y = -x2 - {c * (-1)}";
+                    }
                     else
                     {
                         this.pOgolna.Text = $"y = {a}x2 - {c * (-1)}";
-                    }  
+                    }
                 }
                 else if (c == 0)
                 {
@@ -243,10 +361,14 @@ namespace Abituria
                     {
                         this.pOgolna.Text = $"y = x2";
                     }
+                    else if (a == -1)
+                    {
+                        this.pOgolna.Text = $"y = -x2";
+                    }
                     else
                     {
                         this.pOgolna.Text = $"y = {a}x2";
-                    } 
+                    }
                 }
             }
         }
