d23ddd22501fae37b8a556f4cdac70dfb36a2ff6
haribo841 <adamwaldemarkubis@gmail.com>
2021-11-14 19:34:59 +0100

    Obcy: Przebudzenie

--- Abituria/Abituria szkielet/obj/Debug/Abituria_Content.g.i.cs
+++ Abituria/Abituria szkielet/obj/Debug/Abituria_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/mp21z17.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("img/mp21z33.png")]
 
 
