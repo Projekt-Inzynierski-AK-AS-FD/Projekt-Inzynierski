c397a9a3548358e3ef83199ce887d339a7081f92
Aleksandra <aleksastolow@gmail.com>
2021-11-28 20:09:08 +0100

    mergowanie commitów zepsuło mi wszystkie pliki i musiałam  robić je na nowo, RIP, gosia z rodu magical

--- Abituria/Abituria szkielet/MainWindowLogin.xaml.cs
+++ Abituria/Abituria szkielet/MainWindowLogin.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Navigation;
 using System.IO;
 
 namespace Abituria
@@ -18,50 +19,122 @@ namespace Abituria
     /// <summary>
     /// Logika interakcji dla klasy MainWindowLogin.xaml
     /// </summary>
+    /// 
     public partial class MainWindowLogin : Window
     {
+        string usersFile = @"users.txt";
+
         public MainWindowLogin()
         {
             InitializeComponent();
-            Login();
         }
-        private void Login()
+
+        private List<string> SetUsersList(string usersFile)
         {
-            string path = @"users.txt";
+            //comboBox1.ItemsSource = new List<string> {};
+            List<string> usersList = new List<string>();
 
-
-            using (StreamWriter writer = File.AppendText(path))
+            using (StreamReader reader = new StreamReader(usersFile))
             {
-                //writer.WriteLine("\n heeeeh?");
-                //writer.Close();
+                string user = "";
+                int i = 0;
+
+                while ((user = reader.ReadLine()) != null || i > 9)
+                {
+                    usersList.Add(user);
+                    i++;
+                }
             }
 
-            comboBox1.ItemsSource = new List<string> { "ty mi", "piętnastaka", "dajesz jeżyco?" };
-
-            
-            
-
-
-        }
-        private static void CreateProfile()
-        {
-
+            comboBox1.ItemsSource = usersList;
+            return usersList;
         }
 
         private void BtnAcntExists(object sender, RoutedEventArgs e)
         {
+            btn1.Visibility = Visibility.Collapsed;
+            btn2.Visibility = Visibility.Collapsed;
+            btnConfirm.Visibility = Visibility.Visible;
+            comboBox1.Visibility = Visibility.Visible;
 
+            SetUsersList(usersFile);
         }
 
+        private string ChosenUsername()
+        {
+            string username = comboBox1.SelectedItem as string;
+            return username;
+        }
+
+        private void LoginConfirm(object sender, RoutedEventArgs e)
+        {
+            string username = ChosenUsername();
+            MessageBox.Show(username);
+
+            //do zmiany na lepsze, gdy ustawi się frame'y
+            var mainWin = new MainWindow();
+            mainWin.Owner = this;
+            this.Hide();
+            mainWin.ShowDialog();
+        }
+
+        private void CreateProfile(string newUser, string usersFile, bool isValid)
+        {
+            MessageBox.Show(newUser);
+            using (StreamWriter writer = File.AppendText(usersFile))
+            {
+                if (isValid == true)
+                {
+                    writer.WriteLine(newUser);
+                }
+            }
+        }
         private void BtnCreateNew(object sender, RoutedEventArgs e)
         {
-
+            btn1.Visibility = Visibility.Collapsed;
+            btn2.Visibility = Visibility.Collapsed;
+            inputGB.Visibility = Visibility.Visible;
         }
 
-        private void Confirm(object sender, RoutedEventArgs e)
+        private void AddUser(object sender, RoutedEventArgs e)
         {
-            string eh = comboBox1.SelectedItem as string;
-            MessageBox.Show(eh);
+            string newUsername = nameInput.Text;
+            List<string> userslist = SetUsersList(usersFile);
+            bool isTaken = true;
+            bool isValid = false;
+
+            //dokończyć weryfikację
+            foreach (string user in userslist)
+            {
+                if (user == newUsername)
+                {
+                    MessageBox.Show("Taki użytkownik już istnieje! Wybierz inną nazwę użytkownika", "Nazwa zajęta");
+                    isTaken = true;
+                    break;
+                }
+                else
+                {
+                    isTaken = false;
+                }
+            }
+
+            if (newUsername.Length > 15)
+            {
+                MessageBox.Show("Wybrana nazwa jest za długa!", "Nazwa zbyt długa");
+                isValid = false;
+            }
+            else
+            {
+                if (isTaken == false)
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    isValid = false;
+                }
+            }
+            CreateProfile(newUsername, usersFile, isValid);
         }
     }
 }
