0b71cc73426f65eb6c9392c8219df64daf02cc18
Adam Kubiś <adamwaldemarkubis@gmail.com>
2021-12-04 15:50:26 +0100

    A New Hope
    wszystko już będzie dobrze

---
+++ Abituria szkielet/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Abituria
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    /// koty to mlemlinki
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCalculator(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("CalculatorChoice.xaml", UriKind.Relative);
+            window.Show();
+            this.Visibility = Visibility.Hidden;
+        }
+        private void ButtonAbituria(object sender, RoutedEventArgs e)
+        {
+            var mainWin = new MainWindowLogin();
+            mainWin.Show();
+
+        }
+        private void ButtonMatura(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("PageMaturaLata.xaml", UriKind.Relative);
+            window.Show();
+            this.Visibility = Visibility.Hidden;
+
+            //Ta linijka będzie przydatna, gdy zamiast main window będzie uruchamiało się main page, bo w tej formie ukrywa całe okno
+            //Application.Current.MainWindow.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonDzialy(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("PageDzialyWybor.xaml", UriKind.Relative);
+            window.Show();
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonZadania(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("PageZadania.xaml", UriKind.Relative);
+            window.Show();
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonWzory(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("PageWzory.xaml", UriKind.Relative);
+            window.Show();
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
