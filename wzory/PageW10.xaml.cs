d0862a9be173ffe6a5b2ec895f419d73a1a675e4
Aleksandra <aleksastolow@gmail.com>
2021-12-30 13:23:50 +0100

    to trzeba na spokojnie usiąść

--- wzory/PageW10.xaml.cs
+++ wzory/PageW10.xaml.cs
@@ -24,6 +24,113 @@ namespace Abituria.wzory
         public PageW10()
         {
             InitializeComponent();
+            List<string> chaptersList = new List<string>()
+            {
+                "Wartość Bezwzględna", "Potęgi i pierwiastki", "Logarytmy", "Silnia. Współczynnik dwumianowy", "Dwumian Newtona", "Wzory skróconego mnożenia", "Ciągi", "Funkcja kwadratowa", "Geometria", "Planimetria", "Stereometria", "Trygonometria", "Kombinatoryka", "Prawdopodobieństwo", "Statystyka", "Granice", "Pochodne", "Tablica wartości funkcji trygonometrycznych"
+            };
+            cbChapters.ItemsSource = chaptersList;
+        }
+        void ChangePage(object sender, SelectionChangedEventArgs args)
+        {
+            string selectedItem = cbChapters.SelectedItem.ToString();
+            switch (selectedItem)
+            {
+                case "Wartość Bezwzględna":
+                    PageW1 pageW1 = new PageW1();
+                    NavigationService.Navigate(pageW1);
+                    cbChapters.Text = selectedItem;
+                    cbChapters.SelectedIndex = 0;
+                    break;
+                case "Potęgi i pierwiastki":
+                    PageW2 pageW2 = new PageW2();
+                    NavigationService.Navigate(pageW2);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Logarytmy":
+                    PageW3 pageW3 = new PageW3();
+                    NavigationService.Navigate(pageW3);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Silnia. Współczynnik dwumianowy":
+                    PageW4 pageW4 = new PageW4();
+                    NavigationService.Navigate(pageW4);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Dwumian Newtona":
+                    PageW5 pageW5 = new PageW5();
+                    NavigationService.Navigate(pageW5);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Wzory skróconego mnożenia":
+                    PageW6 pageW6 = new PageW6();
+                    NavigationService.Navigate(pageW6);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Ciągi":
+                    PageW7 pageW7 = new PageW7();
+                    NavigationService.Navigate(pageW7);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Funkcja kwadratowa":
+                    PageW8 pageW8 = new PageW8();
+                    NavigationService.Navigate(pageW8);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Geometria":
+                    PageW9 pageW9 = new PageW9();
+                    NavigationService.Navigate(pageW9);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Planimetria":
+                    PageW10 pageW10 = new PageW10();
+                    NavigationService.Navigate(pageW10);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Stereometria":
+                    PageW11 pageW11 = new PageW11();
+                    NavigationService.Navigate(pageW11);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Trygonometria":
+                    PageW12 pageW12 = new PageW12();
+                    NavigationService.Navigate(pageW12);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Kombinatoryka":
+                    PageW13 pageW13 = new PageW13();
+                    NavigationService.Navigate(pageW13);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Prawdopodobieństwo":
+                    PageW14 pageW14 = new PageW14();
+                    NavigationService.Navigate(pageW14);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Statystyka":
+                    PageW15 pageW15 = new PageW15();
+                    NavigationService.Navigate(pageW15);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Granice":
+                    PageW16 pageW16 = new PageW16();
+                    NavigationService.Navigate(pageW16);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Pochodne":
+                    PageW17 pageW17 = new PageW17();
+                    NavigationService.Navigate(pageW17);
+                    cbChapters.Text = selectedItem;
+                    break;
+                case "Tablica wartości funkcji trygonometrycznych":
+                    PageW18 pageW18 = new PageW18();
+                    NavigationService.Navigate(pageW18);
+                    cbChapters.Text = selectedItem;
+                    break;
+                default:
+                    PageWzory pageWzory = new PageWzory();
+                    NavigationService.Navigate(pageWzory);
+                    break;
+            }
         }
         private void ButtonAbituria(object sender, RoutedEventArgs e)
         {
@@ -55,96 +162,6 @@ namespace Abituria.wzory
         {
             PageWzory pageWzory = new PageWzory();
             NavigationService.Navigate(pageWzory);
-        }
-        private void W1(object sender, RoutedEventArgs e)
-        {
-            PageW1 pageW1 = new PageW1();
-            NavigationService.Navigate(pageW1);
-        }
-        private void W2(object sender, RoutedEventArgs e)
-        {
-            PageW2 pageW2 = new PageW2();
-            NavigationService.Navigate(pageW2);
-        }
-        private void W3(object sender, RoutedEventArgs e)
-        {
-            PageW3 pageW3 = new PageW3();
-            NavigationService.Navigate(pageW3);
-        }
-        private void W4(object sender, RoutedEventArgs e)
-        {
-            PageW4 pageW4 = new PageW4();
-            NavigationService.Navigate(pageW4);
-        }
-        private void W5(object sender, RoutedEventArgs e)
-        {
-            PageW5 pageW5 = new PageW5();
-            NavigationService.Navigate(pageW5);
-        }
-        private void W6(object sender, RoutedEventArgs e)
-        {
-            PageW6 pageW6 = new PageW6();
-            NavigationService.Navigate(pageW6);
-        }
-        private void W7(object sender, RoutedEventArgs e)
-        {
-            PageW7 pageW7 = new PageW7();
-            NavigationService.Navigate(pageW7);
-        }
-        private void W8(object sender, RoutedEventArgs e)
-        {
-            PageW8 pageW8 = new PageW8();
-            NavigationService.Navigate(pageW8);
-        }
-        private void W9(object sender, RoutedEventArgs e)
-        {
-            PageW9 pageW9 = new PageW9();
-            NavigationService.Navigate(pageW9);
-        }
-        private void W10(object sender, RoutedEventArgs e)
-        {
-            PageW10 pageW10 = new PageW10();
-            NavigationService.Navigate(pageW10);
-        }
-        private void W11(object sender, RoutedEventArgs e)
-        {
-            PageW11 pageW11 = new PageW11();
-            NavigationService.Navigate(pageW11);
-        }
-        private void W12(object sender, RoutedEventArgs e)
-        {
-            PageW12 pageW12 = new PageW12();
-            NavigationService.Navigate(pageW12);
-        }
-        private void W13(object sender, RoutedEventArgs e)
-        {
-            PageW13 pageW13 = new PageW13();
-            NavigationService.Navigate(pageW13);
-        }
-        private void W14(object sender, RoutedEventArgs e)
-        {
-            PageW14 pageW14 = new PageW14();
-            NavigationService.Navigate(pageW14);
-        }
-        private void W15(object sender, RoutedEventArgs e)
-        {
-            PageW15 pageW15 = new PageW15();
-            NavigationService.Navigate(pageW15);
-        }
-        private void W16(object sender, RoutedEventArgs e)
-        {
-            PageW16 pageW16 = new PageW16();
-            NavigationService.Navigate(pageW16);
-        }
-        private void W17(object sender, RoutedEventArgs e)
-        {
-            PageW17 pageW17 = new PageW17();
-            NavigationService.Navigate(pageW17);
-        }
-        private void W18(object sender, RoutedEventArgs e)
-        {
-            PageW18 pageW18 = new PageW18();
-            NavigationService.Navigate(pageW18);
         }
     }
 }
